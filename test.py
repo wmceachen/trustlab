3d0df3bfb1057010cec8c4589bddc206ceb25b12
William McEachen <wmceachen@berkeley.edu>
2021-03-17 17:59:00 -0700

    loading in files

--- test.py
+++ test.py
@@ -1,146 +1,12 @@
 import gzip
 import re
+import requests
 from collections import Counter
+from utils import *
 #https://economictimes.indiatimes.com/definition/category/Economy
 # URL_MATCH = '(?<=WARC-Target-URI: ).*'
 # # URL_MATCH = 'WARC-Target-URI:'
-ECON_WORDS = ['GDP',     
-    'Asset Turnover Ratio',
-    'Bailout',
-    'Balance Of Payment',
-    'Bank Rate',
-    'Base Rate',
-    'Broad Money To Reserve Money',
-    'Call Money Rate',
-    'Capacity Cost',
-    'Capital Account',
-    'Capital Adequacy Rati,o'
-    'Capital Market',
-    'Ceteris Paribus',
-    'Clearing Price',
-    'Consumer Price Index',
-    'Consumer Surplus',
-    'Contagion',
-    'Contractionary Policy,'
-    'Core Inflation',
-    'Cost Benefit Analysis,'
-    'Cost Push Inflation',
-    'Countervailing Duties,'
-    'Credit Default Swaps',
-    'Cross Elasticity Of Demand',
-    'Crowding Out Effect',
-    'Currency Deposit Rati,o'
-    'Deadweight Loss',
-    'Debt Equity Ratio',
-    'Deflation',
-    'Depreciation',
-    'Depression',
-    'Dividend Signaling',
-    'Domestic Institutiona,l Invest'
-    'Due Date Rate',
-    'Ease Of Doing Busines,s'
-    'Etf',
-    'Exchange Rate',
-    'Fair Trade Price',
-    'Fallout Risk',
-    'Gross Domestic Saving,'
-    'Gross National Produc,t'
-    'Human Development Ind,ex'
-    'Imperfect Competition,'
-    'Indifference Curve',
-    'Inferior Goods',
-    'Infrastructure Invest,ment' 
-    'Investment Banking',
-    'Invisible Hand',
-    'Labour Market',
-    'Law Of Demand',
-    'Law Of Supply',
-    'Libor',
-    'Liquid Asset',
-    'Liquidity',
-    'Liquidity Trap',
-    'Macroeconomics',
-    'Marginal Standing Facility',
-    'Market Capitalization',
-    'Mark To Market',
-    'Mibor',
-    'Microeconomics',
-    'Monetary Policy',
-    'Money Supply',
-    'Monopoly',
-    'Moral Hazard',
-    'Mumbai Interbank Bid Rate', 
-    'Net Interest Income (nii)',
-    'Net National Income',
-    'Non Performing Assets,'
-    'Paradox',
-    'Paradox Of Thrift',
-    'Pareto\'s Efficiency',
-    'Payments Banks',
-    'Percentage Point',
-    'Perfect Competition',
-    'Phillips Curve',
-    'Poverty Trap',
-    'Price Ceiling',
-    'Price Floor',
-    'Price Mechanism',
-    'Principle Agent Problem',
-    'Privatization',
-    'Producer Surplus',
-    'Production Gap',
-    'Production Possibility Fr',
-    'Profitability Index',
-    'Property Tax',
-    'Proportional Tax',
-    'Public Distribution System',
-    'Purchasing Power Parity',
-    'Quantitative Easing',
-    'Quantity Demanded',
-    'Quantity Supplied',
-    'Quantity Theory Of Mo,ney'
-    'Rational Behaviour',
-    'Real Business Cycle Theory',
-    'Real Economic Growth Rate',
-    'Real Gdp At Factor Cost',
-    'Recession',
-    'Recessionary Gap',
-    'Regressive Tax',
-    'Regulatory Risk',
-    'Rent Seeking',
-    'Repo Rate',
-    'Reserve Ratio',
-    'Residual Claimant',
-    'Revealed Preferences',
-    'Reverse Repo Rate',
-    'Satisficing',
-    'Search Costs',
-    'Seasonal Adjustment',
-    'Secondary Market',
-    'Securitization',
-    'Service Tax',
-    'Shareholder Value',
-    'Social Capital',
-    'Soft Currency',
-    'Soft Loans',
-    'Sovereign Risk',
-    'Special Drawing Rights',
-    'Speculation',
-    'Speculative Motive',
-    'Statutory Liquidity Ratio',
-    'Stimulus Package',
-    'Tender Period',
-    'Trade Union',
-    'True Cost Economics',
-    'Tulip Mania',
-    'Underwriting',
-    'Unemployment Trap',
-    'Union Budget',
-    'Velocity Of Circulation',
-    'Venture Capital',
-    'Windfall Gains',
-    ]
-COVID_WORDS = ['covid', 'covid-19', 'pandemic']
-NEWS_WORDS = ['news', 'journal', 'post', 'reporter']
+
 def is_any(string:str, options:[str]):
     return any(o.lower() in string for o in options)
 
@@ -155,12 +21,17 @@ def text_thing(page_text):
         return True
     return False
 
-with gzip.open('CC-MAIN-20201206002041-20201206032041-00719.warc.wet.gz', 'rb') as f:
-    file_content = f.read().decode("utf-8")
-    # print(file_content)
-    # print(type(file_content))
-    # m = re.findall(URL_MATCH, file_content)
-    # print(m)
-    print(len(list(filter(text_thing, file_content.split('WARC-Target-URI: ')))))
+# for url in get_paths()
+#     stream = requests.get(url).content
+#     print()
+s = requests.get('https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-50/segments/1606141753148.92/wet/CC-MAIN-20201206002041-20201206032041-00719.warc.wet.gz', stream=True)
+print(s.content, file=open('text.txt', 'w+'))
+# with gzip.open('CC-MAIN-20201206002041-20201206032041-00719.warc.wet.gz', 'rb') as f:
+#     file_content = f.read().decode("utf-8")
+#     # print(file_content)
+#     # print(type(file_content))
+#     # m = re.findall(URL_MATCH, file_content)
+#     # print(m)
+#     print(len(list(filter(text_thing, file_content.split('WARC-Target-URI: ')))))
     
 #iterate through, every time we find a URI reset bools about covid and economy, then look for next
